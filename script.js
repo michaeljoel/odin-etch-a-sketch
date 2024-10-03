8dcce9724cdedc00d02b818792fdde722f5ce62b
michaeljoel <michaeljoelatkinson@gmail.com>
2024-10-03 14:30:37 -0500

    Change the class of the grid items

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function generateGrid() {
   const gridContainer = document.querySelector(".grid-container");
   for (let i = 0; i < 16; i++) {
     const gridItem = document.createElement("div");
-    gridItem.classList.add(`gridItem${i}`);
+    gridItem.classList.add(`grid-item`);
     gridContainer.appendChild(gridItem);
   }
 }
