347fbcf9a54e73a3b26731f1184582538d92d99c
michaeljoel <michaeljoelatkinson@gmail.com>
2024-10-03 14:28:35 -0500

    Create a function that generates grid item

---
+++ script.js
@@ -0,0 +1,10 @@
+function generateGrid() {
+  const gridContainer = document.querySelector(".grid-container");
+  for (let i = 0; i < 16; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add(`gridItem${i}`);
+    gridContainer.appendChild(gridItem);
+  }
+}
+
+generateGrid();
