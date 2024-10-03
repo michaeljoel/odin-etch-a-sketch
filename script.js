a14b7e70ba03d3e8812c1988d8c4179a8bb1d675
michaeljoel <michaeljoelatkinson@gmail.com>
2024-10-03 15:58:24 -0500

    Refactor generateGrid function to generate a 16x16 grid

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function generateGrid() {
   const gridContainer = document.querySelector(".grid-container");
   for (let i = 0; i < 16; i++) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add(`grid-item`);
-    gridContainer.appendChild(gridItem);
+    const gridRow = document.createElement("div");
+    gridRow.classList.add("grid-row");
+    gridContainer.appendChild(gridRow);
+    for (let x = 0; x < 16; x++) {
+        const gridColumn = document.createElement("div");
+        gridColumn.classList.add('grid-col');
+        gridRow.appendChild(gridColumn);
+
+        const gridItem = document.createElement("div");
+        gridItem.classList.add(`grid-item`);
+        gridColumn.appendChild(gridItem);
+    }
   }
 }
 
